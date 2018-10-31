e8b08d0ceb07f04473221b069728e0cfc1bd3209
Henadzi Zubaryk <h.zubaryk@godeltech.com>
2018-10-31 11:57:27 +0300

    init

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
-  const getScrollPosition = () => {
-    let rootElement = document.documentElement;
-    let offset = rootElement.scrollTop + window.innerHeight;
-    let height = rootElement.offsetHeight;
-    if (offset >= height) {
-      console.log("At the bottom");
-    } else if (rootElement.scrollTop === 0) {
-      console.log("At the top");
-    }
-  };
-
-  document
-    .getElementsByClassName("check")[0]
-    .addEventListener("click", getScrollPosition);
-});
+const getScrollPosition = () => {
+  let rootElement = document.documentElement;
+  let offset = rootElement.scrollTop + window.innerHeight;
+  let height = rootElement.offsetHeight;
+  if (offset >= height) {
+    console.log("At the bottom");
+  } else if (rootElement.scrollTop === 0) {
+    console.log("At the top");
+  }
+};
